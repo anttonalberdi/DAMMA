65fa739937bbd46bac923f681068f2cd11ecf304
anttonalberdi <anttonalberdi@gmail.com>
2022-06-20 05:06:05 +0200

    Data fix

--- R/function_table.R
+++ R/function_table.R
@@ -3,8 +3,6 @@
 #' Information compiled from databases KEGG, MetaSys and MEROPS.
 #'
 #' Data created from version 20220619
-#' function_table <- read.table("data/DAMMA_functions_20220619.tsv", header=TRUE, sep="\t")
-#' save(function_table, file="data/function_table.RData")
 #'
 #' @docType data
 #' @usage data(function_table)
