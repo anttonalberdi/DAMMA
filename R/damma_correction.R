f0518afaa8dd7184c78629bfe83eada3d69fa1fc
anttonalberdi <anttonalberdi@gmail.com>
2022-08-23 11:17:46 +0200

    Damma_correction modifications

--- R/damma_correction.R
+++ R/damma_correction.R
@@ -1,62 +1,65 @@
 #' Models the relationship between function fullness and MAG completeness and applies a correction factor
 #'
-#' @param fullness_table Table containing fullness scores of functions (X axis) per MAG (Y axis)
+#' @param MCI_table Table containing MCI scores outputed by damma()
 #' @param genome_completeness A matrix containing MAG names (1st column) and completemess values (2nd column)
 #' @param stats Whether to print correction statistics on screen or not. Default=TRUE
 #' @return A corrected fullness table
 #' @import tidyverse
 #' @examples
-#' damma_correction(fullness_table, genome_completeness)
+#' damma_correction(MCI_table, genome_completeness)
 #' @export
 
-damma_correction <- function(fullness_table,genome_completeness,stats=TRUE){
+damma_correction <- function(MCI_table,genome_completeness,stats=TRUE){
 
-  #### UNDER DEVELOPMENT ####
   Genome_completeness <- as.numeric(genome_completeness[,2])
 
+  #Sort Genomes and test matching
+  MCI_table <- MCI_table[order(rownames(MCI_table)),]
+  genome_completeness <- genome_completeness[order(genome_completeness[,1]),]
+  if(all(as.character(rownames(MCI_table)) != as.character(genome_completeness[,1]))) stop("Pathway table is missing")
+
   #Create corrected fullness matrix
-  fullness_table_corrected <- matrix(0,nrow = nrow(fullness_table),ncol = ncol(fullness_table))
-  colnames(fullness_table_corrected)=colnames(fullness_table)
-  rownames(fullness_table_corrected)=rownames(fullness_table)
+  MCI_table_corrected <- matrix(0,nrow = nrow(MCI_table),ncol = ncol(MCI_table))
+  colnames(MCI_table_corrected)=colnames(MCI_table)
+  rownames(MCI_table_corrected)=rownames(MCI_table)
 
   #Iterate modelling and correction for each function
   suppressWarnings(
-    for(i in 1:ncol(fullness_table)){
-      # Arbitrary number of 10 steps is used as weights
-      Model <- glm(fullness_table[,i]~Genome_completeness,family = "binomial")
+    for(i in 1:ncol(MCI_table)){
+      Model <- glm(MCI_table[,i]~Genome_completeness,family = "binomial")
       slope_coef <- coef(Model)[2]
       if(slope_coef > 0){
-        for(j in 1:nrow(fullness_table)){
+        for(j in 1:nrow(MCI_table)){
           # Model prediction of fullness if completeness was 100%
           pred_100 <- round(predict(Model,newdata = data.frame(Genome_completeness=100),type = "response"),1)
           # Model prediction of fullness for actual completeness of the focal MAG
           pred_focal <- round(predict(Model,newdata = data.frame(Genome_completeness=Genome_completeness[j]),type = "response"),1)
           # The expected change in function fullness if focal MAG was 100% complete
           pred_diff <- pred_100-pred_focal
-          fullness_table_corrected[j,i] = fullness_table[j,i]+pred_diff
+          MCI_table_corrected[j,i] = MCI_table[j,i]+pred_diff
         }
       }else if(slope_coef <= 0){
-        fullness_table_corrected[,i] <- fullness_table[,i]
+        MCI_table_corrected[,i] <- MCI_table[,i]
       }
     }
   )
 
   # If corrected fullness >1, convert it to 1.
-  fullness_table_corrected[fullness_table_corrected>1] <- 1
+  MCI_table_corrected[MCI_table_corrected>1] <- 1
 
   #Outout overall correction statistics on screen
-  total <- nrow(fullness_table)*ncol(fullness_table)
-  changes <- c(fullness_table == fullness_table_corrected)
+  total <- nrow(MCI_table)*ncol(MCI_table)
+  changes <- c(MCI_table == MCI_table_corrected)
   changes <- length(changes[!(changes)])
   percentage <- round(changes / total * 100,1)
   cat(paste0(changes," out of ",total," (",percentage,"%) fullness values were corrected\n"))
 
   if(stats == TRUE){
     #Outout overall correction statistics on screen
-    total <- ncol(fullness_table_corrected)
-    for(r in rownames(fullness_table_corrected)){
+    total <- ncol(MCI_table_corrected)
+    for(r in rownames(MCI_table_corrected)){
         completeness <- round(as.numeric(genome_completeness[genome_completeness[,1] == r,2]),1)
-        changes <- fullness_table[r,] == fullness_table_corrected[r,]
+        changes <- MCI_table[r,] == MCI_table_corrected[r,]
         changes <- length(changes[!(changes)])
         percentage <- round(changes / total * 100,1)
         cat(paste0("\t",r," (",completeness,"%): ",changes,"/",total," (",percentage,"%) fullness values were corrected\n"))
@@ -64,6 +67,6 @@ damma_correction <- function(fullness_table,genome_completeness,stats=TRUE){
   }
 
   #Output corrected table
-  return(fullness_table_corrected)
+  return(MCI_table_corrected)
 
 }
