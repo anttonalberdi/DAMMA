b2989560902746fc78ff3c647a1d895544a1b69c
anttonalberdi <anttonalberdi@gmail.com>
2022-11-01 10:29:50 +0100

    Coding system updates

--- R/dama_functions.R
+++ R/dama_functions.R
@@ -11,27 +11,15 @@
 #' @export
 
 
-damma_functions <- function(MCI_table,pathway_table){
+damma_functions <- function(compounds_table,pathway_table){
 
-  #Detect input type
-  if(any(colnames(MCI_table) %in% pathway_table$Code)){level="P"}
-  if(any(colnames(MCI_table) %in% pathway_table$Compound)){level="C"}
-
-  if(level == "P"){
-    MCI_table <- damma_compounds(MCI_table,pathway_table)
-  }else if(level == "C"){
-    MCI_table <- MCI_table
-  }else{
-    stop("MCI table and pathway table do not coincide")
-  }
-
-  functions_table <- t(as.data.frame(t(MCI_table)) %>%
-    rownames_to_column('Compound') %>%
-    left_join(pathway_table[,c('Compound', 'Function')], by = 'Compound') %>%
-    group_by(Function) %>%
+  functions_table <- t(as.data.frame(t(compounds_table)) %>%
+    rownames_to_column('Code_compound') %>%
+    left_join(pathway_table[,c('Code_compound', 'Code_function')], by = 'Code_compound') %>%
+    group_by(Code_function) %>%
     summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
-    arrange(factor(Function, levels = unique(pathway_table$Function))) %>%
-    column_to_rownames('Function'))
+    arrange(factor(Code_function, levels = unique(pathway_table$Code_function))) %>%
+    column_to_rownames('Code_function'))
 
   return(functions_table)
 }
